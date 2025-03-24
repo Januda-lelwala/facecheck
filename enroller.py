a996dac0ce304bc054cfb82c7234cec389a44c7a
{{your_name}} <{{your_email}}>
2025-03-24 11:17:09 +0530

    fourth commit

--- enroller.py
+++ enroller.py
@@ -5,14 +5,8 @@ enroll_person = facecheck.enroll_person
 recognize_person = facecheck.recognize_person
 
 # # Enroll a person
-# enroll_person("test.jpeg", "Januda Lelwala")
-# enroll_person("test2.jpeg", "Sasith Induwara")
-# enroll_person("hdataset/rusiru.jpeg", "Rusiru ")
+# enroll_person("test.jpeg", "James")
+
 # # Recognize a person
-# print(recognize_person("test.jpeg"))
 # print(recognize_person("test2.jpeg"))
-# print(recognize_person("test3.jpeg"))
-# print(recognize_person("test5.jpeg"))
-# print(recognize_person("hdataset/januda.jpeg"))
-# print(recognize_person("hdataset/sasith.jpeg"))
-# print(recognize_person("test4.jpeg"))
+
